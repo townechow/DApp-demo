8e7d844a9901f59ae3c0398f226744eb7a116ba0
datang <35532710+townechow@users.noreply.github.com>
2025-04-25 11:33:52 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["lib/**"], // 👈 忽略 prisma
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
